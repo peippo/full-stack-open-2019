6f17754012d1a1b1e00865b9bcef97d14f7bbc15
Janne Peippo <peippo@lumous.com>
2019-08-18 17:24:54 +0300

    Add osa 2c 2.11 answer

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456" },
-		{ name: "Ada Lovelace", number: "39-44-5323523" },
-		{ name: "Dan Abramov", number: "12-43-234345" },
-		{ name: "Mary Poppendieck", number: "39-23-6423122" }
-	]);
+	const [persons, setPersons] = useState([]);
 	const [filteredPersons, setFilteredPersons] = useState(persons);
 	const [searchTerm, setSearchTerm] = useState("");
 	const [newName, setNewName] = useState("");
@@ -27,6 +23,14 @@ const App = () => {
 		setSearchTerm(event.target.value);
 	};
 
+	// fetch initial persons data from server
+	useEffect(() => {
+		axios.get("http://localhost:3001/persons").then(response => {
+			setPersons(response.data);
+		});
+	}, []);
+
+	// Filter persons based on searchTerm
 	useEffect(() => {
 		const filtered = persons.filter(person =>
 			person.name.toLowerCase().includes(searchTerm.toLowerCase())
